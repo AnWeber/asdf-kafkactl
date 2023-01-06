8d9a3d03e85a0a38dd872909a5ee598ed6a3b4dd
Andreas Weber <weber.andreas@gmail.com>
2023-01-06 16:49:12 +0100

    fix: getArch supports correct version #1

--- lib/utils.bash
+++ lib/utils.bash
@@ -29,17 +29,16 @@ list_all_versions() {
 }
 
 get_arch() {
-  arch=$(uname -m | tr '[:upper:]' '[:lower:]')
-  case ${arch} in
-  armv7l)
-    arch='arm'
-    ;;
-  x86_64)
-    arch='386'
-    ;;
+  ARCH=$(uname -m | tr '[:upper:]' '[:lower:]')
+  case $ARCH in
+    armv*) ARCH="arm64";;
+    aarch64) ARCH="arm64";;
+    x86) ARCH="386";;
+    x86_64) ARCH="amd64";;
+    i686) ARCH="386";;
+    i386) ARCH="386";;
   esac
-
-  echo "${arch}"
+  echo "$ARCH"
 }
 
 get_platform() {
